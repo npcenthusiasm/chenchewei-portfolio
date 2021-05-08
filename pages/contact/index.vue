02e7abb0c30a81cf9e1f72d600f86c426470a5ce
npcenthusiasm <npcenthusiasm@outlook.com>
2021-05-08 18:50:13 +0800

    add concat page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,7 +1,24 @@
 <template>
-  <div>
-    contact
-  </div>  
+  <div class="container mx-auto">
+    <div class="hidden md:block" style="height: 150px"></div>
+    <div class="mx-7 py-4 md:py-12">
+      <div class="flex flex-col md:flex-row">
+        <div class="w-full md:w-4/12">
+        <img src="" alt="" width="120" height="120" style="background-color:#f5f5f5">
+        </div>
+        <div class="w-full md:w-8/12 mt-14 md:mt-0">
+          <p>chewei.smile@gmail.com</p>
+          <hr class="mx-0 my-11 md:my-4 bg-customgray">
+          <ul class="flex flex-wrap">
+            <li class="mr-8 md:mr-10">Facebook</li>
+            <li class="mr-8 md:mr-10">Instagram</li>
+            <li class="mr-8 md:mr-10">Behance</li>
+            <li class="mr-8 md:mr-10">Medium</li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
