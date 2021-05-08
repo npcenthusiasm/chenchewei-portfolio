4ddfc0e2371757cbcd82caa902f6c3be8d76f294
npcenthusiasm <npcenthusiasm@outlook.com>
2021-05-08 17:48:57 +0800

    add burger menu & update navbar

---
+++ components/Burger.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <div class="relative sm:max-w-xl mx-auto">
+      <nav>
+        <button class="text-gray-500 w-10 h-10 relative focus:outline-none" @click="toggle">
+          <div class="block w-5 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
+            <span aria-hidden="true"
+              class="block absolute h-px w-5 bg-current transform transition duration-200 ease-in-out"
+              :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
+            <span aria-hidden="true"
+              class="block absolute h-px w-5 bg-current transform transition duration-200 ease-in-out"
+              :class="{'opacity-0': open } "></span>
+            <span aria-hidden="true"
+              class="block absolute h-px w-5 bg-current transform transition duration-200 ease-in-out"
+              :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
+          </div>
+        </button>
+      </nav>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      open: {
+        type: Boolean
+      }
+    },
+    methods: {
+      toggle() {
+        this.$emit('toggle')
+      }
+    }
+  }
+
+</script>
+
+<style>
+
+</style>
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,16 +1,35 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
-  <nav class="text-lg h-21 px-20 flex justify-between items-center">
+  <nav class="text-lg h-21 px-7 md:px-20 flex justify-between items-center">
     <nuxt-link class="text-gray-900 tracking-wide font-medium" to="/">CHEWEI</nuxt-link>
-    <div class="flex">
-      <nuxt-link class="mx-10 text-base" to="/projects">Projects</nuxt-link>
-      <nuxt-link class="mx-10 text-base" to="/about">About</nuxt-link>
-      <nuxt-link class="mx-10 text-base" to="/services">Services</nuxt-link>
-      <nuxt-link class="mx-10 text-base" to="/contact">Contact</nuxt-link>
+    <div class="hidden md:flex" :class="{'open': open}">
+      <div class="flex">
+        <div class="relative group">
+          <nuxt-link class="px-10 text-base" to="/projects">Projects</nuxt-link>
+          <div class="mt-4 flex absolute z-10 opacity-0 transform -translate-y-6  h-0 bg-grey-200
+            group-hover:h-20 group-hover:opacity-100 transition duration-200
+            group-hover:translate-y-0
+          ">
+            <nuxt-link class="pl-10 pr-10 text-base whitespace-nowrap" to="/product-design">Product Design</nuxt-link>
+            <nuxt-link class="pr-10 text-base whitespace-nowrap" to="/interface-design">Interface Design</nuxt-link>
+            <nuxt-link class="pr-10 text-base whitespace-nowrap" to="/logo">Logo</nuxt-link>
+          </div>
+          <div class="fixed max-h-0 h-full  top-16 w-full left-0 right-0 transition-all duration-200 opacity-0 bg-shonwhite
+            group-hover:opacity-100 group-hover:max-h-20"></div>
+        </div>
+        <nuxt-link class="mx-10 text-base" to="/about" @click.native="close">About</nuxt-link>
+        <nuxt-link class="mx-10 text-base" to="/services" @click.native="close">Services</nuxt-link>
+        <nuxt-link class="mx-10 text-base" to="/contact" @click.native="close">Contact</nuxt-link>
+      </div>
+      <div class="flex">
+        <div class="mx-10">Try</div>
+        <div class="ml-10">Read</div>
+      </div>
     </div>
-    <div class="flex">
-      <div class="mx-10">Try</div>
-      <div class="ml-10">Read</div>
+    <div class="flex md:hidden">
+      <Burger
+        :open="open"
+        @toggle="toggle"/>
     </div>
   </nav>
 
@@ -18,11 +37,40 @@
 
 <script>
   export default {
-
+    data() {
+      return {
+        open: false
+      }
+    },
+    methods: {
+      toggle() {
+        this.open = !this.open
+      },
+      close () {
+        this.open = false
+      }
+    }
   }
-
 </script>
 
 <style>
 
+.open {
+  position: fixed;
+  left: 0;
+  right: 0;
+  height: 100%;
+  top: 84px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  background-color: aqua;
+}
+
+.open a {
+  display: block;
+  text-align: center;
+  width: 100%;
+  padding: 20px 0;
+}
 </style>
