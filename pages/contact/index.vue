bcc22c96cfa95fe15d0a04d52f29f2ec4857edce
npcenthusiasm <npcenthusiasm@outlook.com>
2021-05-30 17:46:48 +0800

    update contact page

--- pages/contact/index.vue
+++ pages/contact/index.vue
@@ -1,19 +1,21 @@
 <template>
-  <div class="container mx-auto">
-    <div class="hidden md:block" style="height: 150px"></div>
-    <div class="mx-7 py-4 md:py-12">
+  <div class="container mx-auto max-w-screen-lgs">
+    
+    <!-- <div class="hidden md:block" style="height: 150px"></div> -->
+    <div class="mx-7 md:mx-0 mt-12 md:mt-37 mb-25 md:mb-45">
       <div class="flex flex-col md:flex-row">
-        <div class="w-full md:w-4/12">
-        <img src="" alt="" width="120" height="120" style="background-color:#f5f5f5">
+        <div class="w-full md:w-4/12 mb-15 md:mb-0">
+        <div class="w-30 h-30" style="background-color:#f5f5f5"></div>
+        <!-- <img src="" alt="" width="120" height="120" style="background-color:#f5f5f5"> -->
         </div>
-        <div class="w-full md:w-8/12 mt-14 md:mt-0">
-          <p>chewei.smile@gmail.com</p>
-          <hr class="mx-0 my-11 md:my-4 bg-customgray">
-          <ul class="flex flex-wrap">
-            <li class="mr-8 md:mr-10">Facebook</li>
-            <li class="mr-8 md:mr-10">Instagram</li>
-            <li class="mr-8 md:mr-10">Behance</li>
-            <li class="mr-8 md:mr-10">Medium</li>
+        <div class="w-full md:w-8/12">
+          <p class="text-xl mb-11 md:mb-9">chewei.smile@gmail.com</p>
+          <hr class="mx-0 mb-10 md:mb-6 bg-customgray">
+          <ul class="flex flex-wrap text-base md:text-sm">
+            <li class="mr-8 mb-6 md:mb-0 md:mr-10 last:mr-0 last:mb-0">Facebook</li>
+            <li class="mr-8 mb-6 md:mb-0 md:mr-10 last:mr-0 last:mb-0">Instagram</li>
+            <li class="mr-8 mb-6 md:mb-0 md:mr-10 last:mr-0 last:mb-0">Behance</li>
+            <li class="mr-8 mb-6 md:mb-0 md:mr-10 last:mr-0 last:mb-0">Medium</li>
           </ul>
         </div>
       </div>
