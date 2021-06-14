f1305544f7dfe679b9d21a695e035c3fc60871af
npcenthusiasm <npcenthusiasm@outlook.com>
2021-06-15 00:32:34 +0800

    fix burger menu

--- components/Burger.vue
+++ components/Burger.vue
@@ -5,14 +5,14 @@
         <button class="text-gray-500 w-6 h-6 relative focus:outline-none" @click="toggle">
           <div class="block w-6 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2">
             <span aria-hidden="true"
-              style="top: -2px;"
+              :style="open ? `top: initial` : `top: -2px`"
               class="block absolute h-px w-6 bg-current transform transition duration-200 ease-in-out"
               :class="{'rotate-45': open,' -translate-y-1.5': !open }"></span>
             <span aria-hidden="true"
               class="block absolute h-px w-6 bg-current transform transition duration-200 ease-in-out"
               :class="{'opacity-0': open } "></span>
             <span aria-hidden="true"
-              style="bottom: -2px;"
+              :style="open ? `bottom: initial` : `bottom: -2px`"
               class="block absolute h-px w-6 bg-current transform transition duration-200 ease-in-out"
               :class="{'-rotate-45': open, ' translate-y-1.5': !open}"></span>
           </div>
