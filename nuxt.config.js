0a5dcfe27f4a5f8ceb0da5b7f9281ea81afee421
npcenthusiasm <npcenthusiasm@outlook.com>
2021-06-21 21:56:53 +0800

    update meta title

--- nuxt.config.js
+++ nuxt.config.js
@@ -21,14 +21,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: '陳哲瑋 - 作品集',
+    title: 'Chewei',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: '陳哲瑋，台灣產品設計師。 從事產品規劃及設計。' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
