dcc693f1db4003a644c3dd1864157522ac07c195
npcenthusiasm <npcenthusiasm@gmail.com>
2021-08-29 22:03:22 +0800

    update navbar & mobile nav fixed

--- components/MobileNavbar.vue
+++ components/MobileNavbar.vue
@@ -1,30 +1,39 @@
 <template>
-  <div id="mobile-nav" class="fixed z-10 w-full h-full px-7" style="top:84px" v-show="show">
-    <div class="flex flex-col lg:flex-row mt-14">
-      <div class="nav-li relative group mb-14" :class="showNav1 ? 'active' : ''">
-        <div class="text-xl flex items-center" to="/product-design" @click="toggleNav">
-          <div class="title">Projects</div>
-          <ArrowUp class="ml-3 text-gray-900 w-7 h-7 inline-block transform" :class="showNav1 ? 'rotate-0 text-black-500' : 'rotate-180'" />
-        </div>
-
-        <ul class="mt-10 flex flex-col lg:flex-row" :class="showNav1 ? 'block' : 'hidden'">
-          <li class="mb-8">
-            <nuxt-link class="ml-8 text-lg text-black-900 whitespace-nowrap block" to="/projects/product-design" @click.native="close">Product Design</nuxt-link>
-          </li>
-          <li class="mb-8">
-            <nuxt-link class="ml-8 text-lg text-black-900 whitespace-nowrap block" to="/projects/interface-design" @click.native="close">Interface Design</nuxt-link></li>
-          <li class="mb-8">
-            <nuxt-link class="ml-8 text-lg text-black-900 whitespace-nowrap block" to="/projects/logo-design" @click.native="close">Logo</nuxt-link>
-          </li>
-        </ul>
+  <div id="mobile-nav" class="" v-show="show">
+    <div class="container">
+      <!-- style="top:84px" -->
+      <div class="flex items-center" style="height: 84px">
+        <nuxt-link class="text-lg text-gray-900 tracking-wide font-medium" to="/" style="letter-spacing: 0.8px;" @click.native="$emit('close')">
+          <img loading="lazy" src="@/assets/img/logo.svg" alt="logo" style="height:22px">
+        </nuxt-link>
       </div>
-      <nuxt-link class="mb-14 text-xl" to="/about" @click.native="close">About</nuxt-link>
-      <nuxt-link class="mb-14 text-xl" to="/services" @click.native="close">Services</nuxt-link>
-      <nuxt-link class="mb-14 text-xl" to="/contact" @click.native="close">Contact</nuxt-link>
-    </div>
-    <div class="flex flex-col lg:flex-row">
-      <nuxt-link class="mb-14 text-xl" to="/try" @click.native="close">Try</nuxt-link>
-      <nuxt-link class="mb-14 text-xl" to="/read" @click.native="close">Read</nuxt-link>
+      <div class="flex flex-col lg:flex-row mt-14">
+        <div class="nav-li relative group mb-14" :class="showNav1 ? 'active' : ''">
+          <div class="text-xl flex items-center" to="/product-design" @click="toggleNav">
+            <div class="title">Projects</div>
+            <ArrowUp class="ml-3 text-gray-900 w-7 h-7 inline-block transform" :class="showNav1 ? 'rotate-0 text-black-500' : 'rotate-180'" />
+          </div>
+
+          <ul class="mt-10 flex flex-col lg:flex-row" :class="showNav1 ? 'block' : 'hidden'">
+            <li class="mb-8">
+              <nuxt-link class="ml-8 text-lg text-black-900 whitespace-nowrap block" to="/projects/product-design" @click.native="close">Product Design</nuxt-link>
+            </li>
+            <li class="mb-8">
+              <nuxt-link class="ml-8 text-lg text-black-900 whitespace-nowrap block" to="/projects/interface-design" @click.native="close">Interface Design</nuxt-link></li>
+            <li class="mb-8">
+              <nuxt-link class="ml-8 text-lg text-black-900 whitespace-nowrap block" to="/projects/logo-design" @click.native="close">Logo</nuxt-link>
+            </li>
+          </ul>
+        </div>
+        <nuxt-link class="mb-14 text-xl" to="/about" @click.native="close">About</nuxt-link>
+        <nuxt-link class="mb-14 text-xl" to="/services" @click.native="close">Services</nuxt-link>
+        <nuxt-link class="mb-14 text-xl" to="/contact" @click.native="close">Contact</nuxt-link>
+      </div>
+      <div class="flex flex-col lg:flex-row">
+        <nuxt-link class="mb-14 text-xl" to="/try" @click.native="close">Try</nuxt-link>
+        <nuxt-link class="mb-14 text-xl" to="/read" @click.native="close">Read</nuxt-link>
+      </div>
+
     </div>
   </div>
 </template>
@@ -84,7 +93,13 @@ import  ArrowUp from '@/components/icon/ArrowUp'
 }
 
 #mobile-nav {
-  @apply bg-white w-full h-full left-0 right-0 container;
+  z-index: 10;
+  width: 100%;
+  height: 100%;
+  left: 0;
+  right: 0;
+  position: fixed;
+  background-color: #fff;
 }
 
 .nav-link {
--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,77 +1,95 @@
 <template>
   <!-- This example requires Tailwind CSS v2.0+ -->
-  <div
+  <!-- <div
     :class="open ? 'fixed w-full h-full top-0 z-10' : ''"
-    :style="open ? 'background-color:#ffffff' : 'background-color:#fcfcfc'">
+    :style="open ? 'background-color:#ffffff' : 'background-color:#fcfcfc'"> -->
+    <div>
   <!-- <div class="" :class="open ? 'fixed bg-white top-0 left-0 right-0' : ''" style="background-color:#fcfcfc"> -->
 
-    <div class="container " >
-      <!-- style="max-width: 1440px" -->
-      
-      <nav class="text-base w-full h-21 flex justify-between items-center">
-        <div class="w-4/12">
+    <div class="hidden lg:block desktop-nav" :class="{'hide': !showNav}">
+      <div class="container">
+        <!-- style="max-width: 1440px" -->
+        
+        <nav class="text-base w-full h-21 flex justify-between items-center">
+          <div class="w-4/12">
+            <nuxt-link class="text-lg text-gray-900 tracking-wide font-medium" to="/" style="letter-spacing: 0.8px;" @click.native="close">
+            <!-- CHEWEI -->
+              <img loading="lazy" src="@/assets/img/logo.svg" alt="logo" style="height:22px">
+            </nuxt-link>
+          </div>
+          <div class="w-8/12 flex">
+            <div class="w-1/2 flex justify-start">
+              <ul class="flex">
+                <li class="nav-link">
+                  <nuxt-link class="text-base py-4 mr-19" to="/projects/product-design" @click.native="close">Projects</nuxt-link>
+                  <div class="nav-child">
+                    <ul class="nav-links-child">
+                      <li class="nav-link-child">
+                        <nuxt-link class="text-sm mr-10 whitespace-nowrap" to="/projects/product-design">Product Design</nuxt-link>
+                      </li>
+                      <li class="nav-link-child">
+                        <nuxt-link class="text-sm mr-10 whitespace-nowrap" to="/projects/interface-design">Interface Design</nuxt-link>
+                      </li>
+                      <li class="nav-link-child">
+                        <nuxt-link class="text-sm mr-10 whitespace-nowrap" to="/projects/logo-design">Logo Design</nuxt-link>
+                      </li>
+                    </ul>
+                  </div>
+                  <!-- <div class="fixed max-h-0 h-full  top-16 w-full left-0 right-0 transition-all duration-200 opacity-0 bg-shonwhite
+                    group-hover:opacity-100 group-hover:max-h-20"></div> -->
+                </li>
+                <li class="nav-link">
+                  <nuxt-link class="text-base py-4 mr-19" to="/about" @click.native="close">About</nuxt-link>
+                </li>
+                <li class="nav-link">
+                  <nuxt-link class="text-base py-4 mr-19" to="/services" @click.native="close">Services</nuxt-link>
+                </li>
+                <li class="nav-link">
+                  <nuxt-link class="text-base py-4 mr-19" to="/contact" @click.native="close">Contact</nuxt-link>
+                </li>
+              </ul>
+            </div>
+            <div class="w-1/2 flex justify-end">
+              <ul class="flex">
+                <li class="nav-link">
+                  <nuxt-link class="text-base py-4 mr-19" to="/try" @click.native="close">Try</nuxt-link>
+                </li>
+                <li class="nav-link">
+                  <nuxt-link class="text-base py-4" to="/read" @click.native="close">Read</nuxt-link>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+      </div>
+    </div>
+<!-- 
+    <div class="flex lg:hidden mobile-nav" :class="{'open-mob-menu': open}">
+      <div class="container">
+        <div class="flex justify-between lg:hidden">
           <nuxt-link class="text-lg text-gray-900 tracking-wide font-medium" to="/" style="letter-spacing: 0.8px;" @click.native="close">
-          <!-- CHEWEI -->
             <img loading="lazy" src="@/assets/img/logo.svg" alt="logo" style="height:22px">
           </nuxt-link>
+          <div></div>
         </div>
-        <div class="hidden w-8/12 lg:flex">
-          <div class="w-1/2 flex justify-start">
-            <ul class="flex">
-              <li class="nav-link">
-                <nuxt-link class="text-base py-4 mr-19" to="/projects/product-design" @click.native="close">Projects</nuxt-link>
-                <div class="nav-child">
-                  <ul class="nav-links-child">
-                    <li class="nav-link-child">
-                      <nuxt-link class="text-sm mr-10 whitespace-nowrap" to="/projects/product-design">Product Design</nuxt-link>
-                    </li>
-                    <li class="nav-link-child">
-                      <nuxt-link class="text-sm mr-10 whitespace-nowrap" to="/projects/interface-design">Interface Design</nuxt-link>
-                    </li>
-                    <li class="nav-link-child">
-                      <nuxt-link class="text-sm mr-10 whitespace-nowrap" to="/projects/logo-design">Logo Design</nuxt-link>
-                    </li>
-                  </ul>
-                </div>
-                <!-- <div class="fixed max-h-0 h-full  top-16 w-full left-0 right-0 transition-all duration-200 opacity-0 bg-shonwhite
-                  group-hover:opacity-100 group-hover:max-h-20"></div> -->
-              </li>
-              <li class="nav-link">
-                <nuxt-link class="text-base py-4 mr-19" to="/about" @click.native="close">About</nuxt-link>
-              </li>
-              <li class="nav-link">
-                <nuxt-link class="text-base py-4 mr-19" to="/services" @click.native="close">Services</nuxt-link>
-              </li>
-              <li class="nav-link">
-                <nuxt-link class="text-base py-4 mr-19" to="/contact" @click.native="close">Contact</nuxt-link>
-              </li>
-            </ul>
-          </div>
-          <div class="w-1/2 flex justify-end">
-            <ul class="flex">
-              <li class="nav-link">
-                <nuxt-link class="text-base py-4 mr-19" to="/try" @click.native="close">Try</nuxt-link>
-              </li>
-              <li class="nav-link">
-                <nuxt-link class="text-base py-4" to="/read" @click.native="close">Read</nuxt-link>
-              </li>
-            </ul>
-          </div>
-        </div>
-        <div class="flex lg:hidden">
-          <Burger
-            :open="open"
-            @toggle="toggle"/>
-        </div>
-      </nav>
+      </div>
+    </div> -->
+
+    <div class="flex lg:hidden mobile-nav-burger">
+      <Burger
+        :open="open"
+        @toggle="toggle"
+        @close="close"
+      />
     </div>
+
     <MobileNavbar :show="open" @close="toggle"/>
   </div>
-
 </template>
 
 <script>
 import  ArrowUp from '@/components/icon/ArrowUp'
+import throttle from 'lodash.throttle'
 
   export default {
     components: {
@@ -81,9 +99,44 @@ import  ArrowUp from '@/components/icon/ArrowUp'
       return {
         open: false,
         showNav1: false,
+        showNav: true,
+        scrollY: 0,
+        scrollDeriction: 'down',
+        scrollFn: function () {}
       }
     },
+    beforeMount () {
+      this.scrollFn = throttle(this.handleBodyScroll, 150)
+      window.addEventListener('scroll', this.scrollFn)
+    },
+    beforeDestroy () {
+      window.removeEventListener('scroll', this.scrollFn)
+    },
     methods: {
+      handleBodyScroll (e) {
+
+        if (this.scrollY - window.scrollY < 0) {
+          this.scrollDeriction = 'down'
+          // 往上捲
+        } else if (this.scrollY - window.scrollY > 0) {
+          this.scrollDeriction = 'up'
+        }
+
+        this.scrollY = window.scrollY
+
+        if(this.scrollDeriction === 'up') {
+          this.showNav = true
+        } else {
+          // down
+          if (this.scrollY > 100) {
+            this.showNav =  false
+          } else {
+            this.showNav = true
+          }
+        }
+
+
+      },
       toggle() {
         this.open = !this.open
       },
@@ -98,6 +151,51 @@ import  ArrowUp from '@/components/icon/ArrowUp'
 </script>
 
 <style>
+
+.desktop-nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  background-color:#fcfcfc;
+  transition: transform .3s ease-in-out;
+  transform: translateY(0);
+  z-index: 1000;
+}
+
+.desktop-nav.hide {
+  transform: translateY(-100%);
+}
+
+
+.mobile-nav {
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  height: 84px;
+  width: 100%;
+  z-index: 19999;
+  /* display: flex; */
+  justify-content: center;
+  align-items: center;
+  background-color:transparent;
+
+}
+
+.mobile-nav.open-mob-menu {
+  background-color:transparent;
+}
+
+.mobile-nav-burger {
+  position: fixed;
+  z-index: 19999;
+  top: 30px;
+  right: 28px;
+}
+
+
+
 
 .open {
   position: fixed;
@@ -117,9 +215,6 @@ import  ArrowUp from '@/components/icon/ArrowUp'
   /* color: grey; */
 }
 
-#mobile-nav {
-  @apply bg-white w-full h-full left-0 right-0 container;
-}
 
 .nav-link {
   /* border: 1px solid red; */
